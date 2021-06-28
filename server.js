777de811768dcdf2fbdbe31f86406861cd4120c9
Jeremy's commits <jeremyhurst16@gmail.com>
2021-06-28 02:46:38 -0400

    early commit to change workstations

--- server.js
+++ server.js
@@ -6,8 +6,12 @@ const { animals } = require('./data/animals')
 const PORT = process.env.PORT || 3001
 const app = express()
 
-app.use(express.urlencoded({ extended: true }))
-app.use(express.json())
+// parse incoming string or array data
+app.use(express.urlencoded({ extended: true }));
+// parse incoming JSON data
+app.use(express.json());
+// lets the server know how to display other code
+app.use(express.static('public'));
 
 function filterByQuery(query, animalsArray) {
   let personalityTraitsArray = []
@@ -92,15 +96,21 @@ app.get('/api/animals/:id', (req, res) => {
 
 app.post('/api/animals', (req, res) => {
   // set id based on what the next index of the array will be
-  req.body.id = animals.length.toString()
+  req.body.id = animals.length.toString();
 
+  // if any data in req.body is incorrect, send 400 error back
   if (!validateAnimal(req.body)) {
-    res.status(400).send('The animal is not properly formatted.')
+    res.status(400).send('The animal is not properly formatted.');
   } else {
-    const animal = createNewAnimal(req.body, animals)
-    res.json(animal)
+    const animal = createNewAnimal(req.body, animals);
+    res.json(animal);
   }
-})
+});
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
 app.listen(PORT, () => {
   console.log(`API server now on port ${PORT}!`)
 })
